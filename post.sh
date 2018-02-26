fdbe05f5b25f9524ed5a3205a2a46dfae3f1a1f3
Dmitriy Ivanov <divanov@aligntech.com>
2018-02-26 14:58:29 +0300

    Post 2018-02-26-14:58:29

--- post.sh
+++ post.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+python -m mkdocs gh-deploy --clean
+
 git add --all
 
 git commit -a -m "Post $(date +%Y-%m-%d-%H:%M:%S)"
 
 git push -u origin master
 
-python -m mkdocs gh-deploy --clean
