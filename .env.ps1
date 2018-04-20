d885e206db40520440b9a04ef5acb8299459adc9
Dmitriy Ivanov <d.k.ivanov@live.com>
2018-04-20 15:32:03 +0300

    Post 2018-04-20-15:32:03

--- .env.ps1
+++ .env.ps1
@@ -2,6 +2,6 @@
 # TBD...
 
 python -m pip install virtualenv
-python -m virtualenv -p c:\Python\3.6.3-x64\python.exe .\venv
+python -m virtualenv -p C:\Python36\python.exe .\venv
 .\venv\Scripts\Activate.ps1
 python -m pip install -r .\requirements.txt
--- post.ps1
+++ post.ps1
@@ -1,10 +1,7 @@
-#!/bin/bash
+#requires -version 3 
 
 python -m mkdocs gh-deploy --clean
-
 git add --all
-
 git commit -a -m "Post $(date +%Y-%m-%d-%H:%M:%S)"
-
 git push -u origin master
 
