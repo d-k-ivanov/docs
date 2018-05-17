b53091c73b9bc7ae1bd6648df65852c0032b87f5
Dmitriy Ivanov <d.k.ivanov@live.com>
2018-05-17 11:31:19 +0300

    Post 2018-05-17-11:31:19

--- post.ps1
+++ post.ps1
@@ -2,6 +2,6 @@
 
 python -m mkdocs gh-deploy --clean
 git add --all
-git commit -a -m "Post $(date +%Y-%m-%d-%H:%M:%S)"
+git commit -a -m "Post $(Get-Date -UFormat +%Y-%m-%d-%H:%M:%S)"
 git push -u origin master
 
